53bbaa6f87370937f1ff837a350b3c383ba439d6
djay0507 <djay0507@icloud.com>
2022-03-31 12:27:56 -0500

    added files

---
+++ Shared/CostVariables.swift
@@ -0,0 +1,20 @@
+//
+//  CostVariables.swift
+//  MMS
+//
+//  Created by Hannah Douget on 3/31/22.
+//
+
+import SwiftUI
+
+struct CostVariables: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CostVariables_Previews: PreviewProvider {
+    static var previews: some View {
+        CostVariables()
+    }
+}
---
+++ Shared/TimeVariables.swift
@@ -0,0 +1,20 @@
+//
+//  TimeVariables.swift
+//  MMS
+//
+//  Created by Hannah Douget on 3/31/22.
+//
+
+import SwiftUI
+
+struct TimeVariables: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeVariables_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeVariables()
+    }
+}
